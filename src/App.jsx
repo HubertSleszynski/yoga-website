c398a8dc14b1d09bf5d0e228e0224d9ce112f5dd
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-05-13 15:49:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
+import Facts from "./components/Facts";
+import Features from "./components/Features";
+import Courses from "./components/Courses";
+import Pricing from "./components/Pricing";
+import Newsletter from "./components/Newsletter";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return <div className="overflow-hidden"></div>;
+}
+
+export default App;
